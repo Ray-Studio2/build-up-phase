a3ec2f4c56a0d2b54b782cb54b2a02bbbb87fff0
Nefty <anisam521@gmail.com>
2025-04-03 03:19:14 +0900

    Simple platform frontend is ready. working on MacOS and Windows.

--- Seohyuk/simple_platform/MacOSPlatform/TestCocoaPlatform/main.cpp
+++ Seohyuk/simple_platform/MacOSPlatform/TestCocoaPlatform/main.cpp
@@ -1,19 +1,20 @@
 //
 //  main.cpp
-//  test_window
+//  TestCocoaPlatform
 //
 //  Created by 송서혁 on 4/2/25.
 //
 
+#define _Cocoa
+#include "CocoaPlatform.h"
+
 #include <iostream>
-#include "platform.h"
+
 
 int main(int argc, const char * argv[]) {
     
     
     
-    
-    
-    
+    std::cout << "Hello, World!\n";
     return 0;
 }
--- Seohyuk/test/Test.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-inline void printTest(const std::string &frontMessage, const bool param) {
-    std::cout << frontMessage << " testIs: "  << (param ? "Success" : "Failed") << std::endl;
-}
-
