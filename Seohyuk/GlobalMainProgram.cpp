03971728e59a4e8a3a2ea2c09faf6a9492dd629b
Nefty <anisam521@gmail.com>
2025-04-09 14:01:59 +0900

    no message

--- Seohyuk/GlobalMainProgram.cpp
+++
@@ -1,9 +0,0 @@
-#include "GlobalMainProgram.h"
-
-int main() {
-
-  std::cout << "Hello World!" << std::endl;
-  std::cout << "If you successfully generated cmake files, That means you are ready to go!" << std::endl;
-
-  return 0;
-}
