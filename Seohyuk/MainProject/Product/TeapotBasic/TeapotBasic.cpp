be25d9da638773ce956a40dbfad9136eff3256ee
NEFTY-CUBIC\NeftyOliver <anisam521@gmail.com>
2025-04-17 10:09:09 +0900

    Added bunch of codes.

--- Seohyuk/MainProject/Product/TeapotBasic/TeapotBasic.cpp
+++ Seohyuk/MainProject/Product/TeapotBasic/TeapotBasic.cpp
@@ -3,14 +3,40 @@
 
 
 
-void nutshell::beforeRender() {}
 
 
 void nutshell::whileRendering() {
   //std::cout << "Hello, World!" << std::endl;
 }
-void nutshell::drawCallBackMain(GLFWwindow *pWindow, VkInstance instance, VkDevice device, VkQueue queue) {
+void nutshell::drawCallBackMain(GLFWwindow *pWindow, VkInstance instance, VkDevice device, VkQueue queue, Synchronizer synchronizer) {
+  vkWaitForFences(device, 1, &synchronizer.fence, VK_TRUE, UINT64_MAX);
 
+
+  VkCommandBufferBeginInfo commandBeginInfo = {
+    VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+    nullptr,
+    0,
+    nullptr
+  };
+  vkBeginCommandBuffer(commandBuffer, &commandBeginInfo);
+
+  vkEndCommandBuffer(commandBuffer);
+
+  vkResetCommandBuffer(commandBuffer, 0);
+  vkResetFences(device, 1, &synchronizer.fence);
+
+
+  VkSubmitInfo submitInfo = {
+  VK_STRUCTURE_TYPE_SUBMIT_INFO,
+  nullptr,
+    //pWaitSemaphores
+    //pWaitDstStageMask
+    //commandBufferCount
+    //pCommandBuffers
+    //signalSemaphoreCount
+    //pSignalSemaphores
+  };
+  vkQueueSubmit(queue, 1, &submitInfo, reinterpret_cast<VkFence>(&synchronizer.fence));
 }
 
 
--- Seohyuk/include/vk_nutshell.h
+++ Seohyuk/include/vk_nutshell.h
@@ -2,14 +2,15 @@
 
 #include <iostream>
 #include <memory>
-#include <queue>
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 
 #include "vulkan_utility.h"
 
 #define DEVICE_SELECTION 0
+#define QUEUE_PRIORITY 1.0f
 #define PRINT_INFO
+
 
 
 #ifdef NDEBUG
@@ -39,6 +40,10 @@ inline VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo {
 };
 
 namespace nutshell {
+    typedef struct Synchronizer_ {
+        VkFence fence;
+        VkSemaphore semaphore;
+    } Synchronizer;
 
     /**
      * all the draw callbacks must be implemented allways.
@@ -60,13 +65,9 @@ namespace nutshell {
      *
      */
 
-
-    void (beforeRender)();                                                                                     /* anything before the renderpass starts */
-    //void (drawCallPreRender)(GLFWwindow *pWindow, VkInstance * instance, VkDevice * device, VkQueue * queue);  /* inside the renderpass before something renders */
     void (whileRendering)();                                                                                   /* something to dso in program loop */
-    void (drawCallBackMain)(GLFWwindow *pWindow, VkInstance instance, VkDevice device, VkQueue queue);   /* main rendering stage */
-    //void (drawCallPostRender)(GLFWwindow *pWindow, VkInstance * instance, VkDevice * device, VkQueue * queue); /* after the rendering inside a renderpass */
-    void (afterRedner)();                                                                                      /* last thing to do in main loop */
+    void (drawCallBackMain)(GLFWwindow *pWindow, VkInstance instance, VkDevice device, VkQueue queue, Synchronizer synchronizer);   /* main rendering stage */
+
 
     /**
      * Very simple Vulkan instance context with some device info and the command pool.
@@ -76,15 +77,14 @@ namespace nutshell {
             GLFWwindow * window = nullptr;
             VkSurfaceKHR surface = nullptr;
             VkSwapchainKHR swapChaine = nullptr;
+            std::vector<VkImage> swapChainImages{};
+            std::vector<VkImageView> swapChainImageViews{};
+            std::vector<VkFramebuffer> framebuffer{};
         } PresentationUnit;
 
-        std::vector<const char *> instanceLayerRequestList {};
+        std::vector<const char *> instanceLayerRequestList {
+        };
         std::vector<const char *> instanceExtensionRequestList = {
-            //"VK_KHR_get_physical_device_properties2",
-            //"VK_KHR_get_surface_capabilities2",
-
-            //"VK_EXT_DEBUG_UTILS_EXTENSION_NAME",
-            //"VK_EXT_debug_utils",
             "VK_KHR_portability_enumeration",
 
 #ifdef __APPLE__
@@ -97,17 +97,15 @@ namespace nutshell {
         VkInstance instance;
 
         std::vector<VkPhysicalDevice> physicalDevices{};
-        VkDevice deviceUnique;
+        VkDevice device;
         const float queuePriorities = 1.0;
-        VkQueue queueUnique;
-        VkCommandPool commandPoolUnique;
-        VkCommandBuffer commandBufferUnique;
+        VkQueue queue;
+        VkCommandPool commandPool;
+        //VkCommandBuffer commandBuffer;
 
-
-
+        Synchronizer synchronizer = {};
 
         VkContext_();
-
 
         /**
          * After call this function the program will be started and loops.
@@ -207,21 +205,168 @@ namespace nutshell {
         std::cout << "Physical device selected: " << "number: " << DEVICE_SELECTION  << std::endl << "device name:" << deviceProperties.deviceName << std::endl;
 
 #endif
+
+
+
+        uint32_t queueFamilySelection = vkut::autoSelectQueueFamily(physicalDevices.at(DEVICE_SELECTION), DEVICE_SELECTION);
+
+
+        float priority = QUEUE_PRIORITY;
+        VkDeviceQueueCreateInfo queueCreateInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        nullptr,
+            0,
+            queueFamilySelection,
+            1,
+            &priority
+        };
+
+        std::vector<const char *>  deviceExtensionRequestList = {
+            "VK_KHR_swapchain"
+        };
+
+        VkDeviceCreateInfo deviceCreateInfo {
+            VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+            nullptr,
+            0,
+            1,
+            &queueCreateInfo,
+            0, // deprecated
+            {}, // deprecated
+            static_cast<unsigned int>(deviceExtensionRequestList.size()),
+            deviceExtensionRequestList.data(),
+            {}
+        };
+
+        vkCreateDevice(physicalDevices.at(DEVICE_SELECTION), &deviceCreateInfo, nullptr, &device);
+
+        if (device == VK_NULL_HANDLE) {
+            std::cerr << "Failed to create device" << std::endl;
+        }
+
+        VkCommandPoolCreateInfo cmdPoolCreateInfo {
+        VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+        nullptr,
+            VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+
+        };
+        vkCreateCommandPool(device, &cmdPoolCreateInfo, nullptr, &commandPool);
+
+        if (commandPool == VK_NULL_HANDLE) {
+            std::cerr << "Failed to create command pool" << std::endl;
+        }
+
+        if (glfwCreateWindowSurface(instance, PresentationUnit.window, nullptr, &PresentationUnit.surface)) {
+            std::cerr << "Failed to create surface" << std::endl;
+        }
+
+
+
+        vkut::SurfaceInfo surfaceInfo = vkut::getSurfaceInfo(physicalDevices.at(DEVICE_SELECTION), PresentationUnit.surface, nullptr);
+
+        VkSwapchainCreateInfoKHR swapchainCreateInfo = {
+        VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        nullptr,
+        0,
+        PresentationUnit.surface,
+            2,
+            surfaceInfo.imageFormat,
+            surfaceInfo.imageColorSpace,
+            surfaceInfo.imageExtent,
+            surfaceInfo.imageArrayLayerCount,
+            surfaceInfo.imageUsage,
+            VK_SHARING_MODE_EXCLUSIVE,
+
+            1,
+            &queueFamilySelection,
+            surfaceInfo.preTransform,
+            VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+            VK_PRESENT_MODE_FIFO_KHR,
+            VK_TRUE,
+            VK_NULL_HANDLE,
+
+        };
+        vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &PresentationUnit.swapChaine);
+
+        uint32_t imageCount;
+        vkGetSwapchainImagesKHR(device, PresentationUnit.swapChaine, &imageCount, nullptr);
+        std::vector<VkImage> swapchainImages(imageCount);
+        vkGetSwapchainImagesKHR(device, PresentationUnit.swapChaine, &imageCount, swapchainImages.data());
+
+
+        std::vector<VkImageView> swapchainImageViews(imageCount);
+        for (int i = 0; i < imageCount; i += 1) {
+            VkImageViewCreateInfo swapchainImageViewCreateInfo = {
+                VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+                nullptr,
+                0,
+                swapchainImages.at(i),
+                VK_IMAGE_VIEW_TYPE_2D,
+                surfaceInfo.imageFormat,
+                VK_COMPONENT_SWIZZLE_IDENTITY,
+                VK_COMPONENT_SWIZZLE_IDENTITY,
+                VK_COMPONENT_SWIZZLE_IDENTITY,
+                VK_COMPONENT_SWIZZLE_IDENTITY,
+                VK_IMAGE_ASPECT_COLOR_BIT,
+                0,
+                1,
+                0,
+                1
+            };
+
+            VkImageView swapchainImageView;
+            vkCreateImageView(device, &swapchainImageViewCreateInfo, nullptr, &swapchainImageView);
+
+            swapchainImageViews.push_back(swapchainImageView);
+        }
+
+
+        //RenderPass
+        //Pieline
+
+
+
+        for (uint32_t i = 0; i < imageCount; i += 1) {
+
+            /**
+             *
+            *VkFramebufferCreateFlags    flags;
+    VkRenderPass                renderPass;
+    uint32_t                    attachmentCount;
+    const VkImageView*          pAttachments;
+    uint32_t                    width;
+    uint32_t                    height;
+    uint32_t                    layers;
+             */
+            VkFramebufferCreateInfo swapchainFramebufferCreateInfo = {
+                VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+                nullptr,
+                0,
+                nullptr,
+
+            };
+        }
+
+        VkFenceCreateInfo fenceCreateInfo = {
+        VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+        nullptr,
+            VK_FENCE_CREATE_SIGNALED_BIT
+        };
+        vkCreateFence(device, &fenceCreateInfo, nullptr, &synchronizer.fence);
+
     }
 
 
     inline void VkContext_::programLoop() const {
         while ( !glfwWindowShouldClose(PresentationUnit.window) ) {
-            beforeRender();
+
             {
-                //drawCallPreRender(PresentationUnit.window, instanceUnique.get(), deviceUnique.get(), queueUnique.get());
                 {
                     whileRendering();
-                    drawCallBackMain(PresentationUnit.window, instance, deviceUnique, queueUnique);;
+                    drawCallBackMain(PresentationUnit.window, instance, device, queue, synchronizer);
                 }
-                //drawCallPostRender(PresentationUnit.window, reinterpret_cast<VkInstance>(instanceUnique.get()), *device, *queue);
             }
-            afterRedner();
+
 
             glfwSwapBuffers(PresentationUnit.window);
             glfwPollEvents();
@@ -231,15 +376,105 @@ namespace nutshell {
     }
 
     inline VkContext_::~VkContext_() {
-    /*
-     * in case we are using unique, we don't have to free things manually.
-     */
+        for (const auto imageView: PresentationUnit.swapChainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+
+        for (const auto image: PresentationUnit.swapChainImages) {
+            vkDestroyImage(device, image, nullptr);
+        }
+
+        vkDestroyFence(device, synchronizer.fence, nullptr);
+
+        vkDestroySwapchainKHR(device, PresentationUnit.swapChaine, nullptr);
+
+        vkDestroyCommandPool(device, commandPool, nullptr);
+        vkDestroyDevice(device, nullptr);
+
 
         glfwDestroyWindow(PresentationUnit.window);
+
+
+        //vkDestroySurfaceKHR(instance, PresentationUnit.surface, nullptr);
+        //vkDestroyInstance(instance, nullptr);
 
         std::cout << "Nutshell says goodbye~" << std::endl; // if this doesn't happen, you are doing destroy in wrong way.
     }
 
+
+    typedef struct RenderingInstruction_ {
+        VkDevice device;
+        VkCommandBuffer commandBuffer;
+        VkPipeline pipeline;
+        VkRenderPass renderPass;
+
+
+        void setDevice(VkDevice const *device);
+
+        void allocCommandBuffer(const VkCommandPool * commandPool);
+        void createPipline();
+
+        void createRenderPass(VkFormat swapChainImageFormat) ;
+
+        void cleanup() const;
+    } RenderingInstruction;
+
+    inline void RenderingInstruction_::setDevice(VkDevice const* device) {
+        this->device = *device;
+    }
+
+    inline void RenderingInstruction_::allocCommandBuffer(const VkCommandPool * commandPool) {
+        const VkCommandBufferAllocateInfo commandBufferAllocateInfo = {
+            VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+            nullptr,
+            *commandPool,
+            VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+            1
+        };
+        vkAllocateCommandBuffers(device, &commandBufferAllocateInfo, &commandBuffer);
+    }
+
+    inline void RenderingInstruction_::createPipline() {
+
+    }
+
+    inline void RenderingInstruction_::createRenderPass(const VkFormat swapChainImageFormat) {
+        VkAttachmentDescription colorAttachment{};
+        colorAttachment.format = swapChainImageFormat;
+        colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+
+        VkAttachmentReference colorAttachmentRef = {};
+        colorAttachmentRef.attachment = 0;
+        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+
+        VkSubpassDescription subpass = {};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &colorAttachmentRef;
+
+
+        VkRenderPassCreateInfo renderPassCreateInfo = {
+            VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+        nullptr,
+            0,
+            1,
+            &colorAttachment,
+            1,
+            &subpass,
+            0,
+            nullptr
+        };
+
+        vkCreateRenderPass(device, &renderPassCreateInfo, nullptr, &renderPass);
+    }
+
+    inline void RenderingInstruction_::cleanup() const {
+        vkDestroyRenderPass(device, renderPass, nullptr);
+        vkDestroyPipeline(device, pipeline, nullptr);
+    }
 }
 
 
